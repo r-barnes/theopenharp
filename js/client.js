355093d5a18103f4a3b757e14926784b75a68d68
Richard Barnes <rbarnes@umn.edu>
2013-09-21 18:15:21 -0500

    Added text books. Added drop down page selection.

--- js/client.js
+++ js/client.js
@@ -3,14 +3,52 @@ var panzoom;
 
 function pad(num, size){ return ('000000000' + num).substr(-size); }
 
-function LoadBook(path){
-  $.getJSON('data/'+path+'/book.json', function(data){
-    book=data;
-    book.curpage=1;
-    book.pages='data/'+path+'/'+data.prefix;
-    book.padlen=book.maxpage.toString().length;
-    LoadPage(book.curpage);
-  }).fail(function(){console.log('Failed to get book metadata.');});
+function SetupDropdown(){
+  if(!book.loaded) return;
+
+  var pagedrop=$('#pagedrop');
+
+  pagedrop.show();
+  pagedrop.empty();
+
+  if(book.type=="images"){
+    for(var i=book.minpage;i<=book.maxpage;++i)
+      pagedrop.append('<option value="'+i+'">'+i+'</option>');
+  } else if (book.type=="text") {
+    for(var i=book.minpage;i<=book.maxpage;++i)
+      pagedrop.append('<option value="'+i+'">'+book.pages[i].number+" "+book.pages[i].name+'</option>');
+  }
+}
+
+function LoadBook(path, type){
+  if(type=="images"){
+    $.getJSON('data/'+path+'/book.json', function(data){
+      book=data;
+      book.loaded=true;
+      book.curpage=1;
+      book.minpage=1;
+      book.type="images"
+      book.pages='data/'+path+'/'+data.prefix;
+      book.padlen=book.maxpage.toString().length;
+      $('#textpage').hide();
+      $('#parent').show();
+      SetupDropdown();
+      LoadPage(book.curpage);
+    }).fail(function(){console.log('Failed to get book metadata.');});
+  } else if (type=="text") {
+    $.getJSON('data/lyrics_'+path+'.json', function(data){
+      book.pages=data;
+      book.type="text";
+      book.loaded=true;
+      book.curpage=0;
+      book.minpage=0;
+      book.maxpage=data.length-1;
+      $('#parent').hide();
+      $('#textpage').show();
+      LoadPage(book.curpage);
+      SetupDropdown();
+    }).fail(function(){console.log('Failed to get book text.');});
+  }
 }
 
 function SetupZoom(){
@@ -28,13 +66,22 @@ function SetupZoom(){
 
 function LoadPage(page){
   book.curpage=page;
-  $('#curpage').attr('src',book.pages+pad(page,book.padlen)+book.suffix);
-  SetupZoom();
+  if(book.type=="images"){
+    $('#curpage').attr('src',book.pages+pad(page,book.padlen)+book.suffix);
+    SetupZoom();
+  } else if (book.type=="text") {
+    $('#textpage .name').html(book.pages[book.curpage].name);
+    $('#textpage .number').html(book.pages[book.curpage].number);
+    $('.words').html(book.pages[book.curpage].words);
+    $('.tune').html(book.pages[book.curpage].tune);
+    $('#textpage .lyrics').html(book.pages[book.curpage].lyrics);
+  }
+  $('#pagedrop').val(book.curpage);
 }
 
 function FlipPage(inc){
-  if(!book.name) return;
-  if(book.curpage+inc>0 && book.curpage+inc<=book.maxpage)
+  if(!book.loaded) return;
+  if(book.curpage+inc>=book.minpage && book.curpage+inc<=book.maxpage)
     LoadPage(book.curpage+inc);
   return false;
 }
@@ -43,23 +90,26 @@ $(document).ready(function(){
   var dropdown=$('#booklist');
   $.getJSON("data/books.json", function( data ) {
     $.each(data, function(i,book){
-      dropdown.append('<li><a class="bookchoice" href="#" data-path="'+book.path+'">'+book.name+'</a></li>');
+      dropdown.append('<li><a class="bookchoice" href="#" data-type="'+book.type+'" data-path="'+book.path+'">'+book.name+'</a></li>');
     });
 
     $('.bookchoice').click(function(){
-      LoadBook($(this).data('path'));
+      LoadBook($(this).data('path'), $(this).data('type'));
     });
   }).fail(function(){console.log('Error loading file');});
 
 
   $('#flipleft') .click(function(){ FlipPage(-1); });
   $('#flipright').click(function(){ FlipPage( 1); });
+  $('#pagedrop').change(function(){
+    LoadPage($(this).val());
+  });
   $(document).bind('keydown', function(event){
-    if(!book.name) return;
+    if(!book.loaded) return;
 
     var img = $('#curpage');
     var key = event.which;
-    console.log(key);
+    //console.log(key);
     if(key == 33){        //Page Up
       FlipPage(-1);
       event.stopPropagation();
@@ -70,13 +120,13 @@ $(document).ready(function(){
       return false;
 //    } else if (key==187) { //Plus Key and Equals key
 //    } else if (key==189) { //Minus key
-    } else if (key==40) { //Down key
+    } else if (key==40 && book.type=="images") { //Down key
       $('#panzoom').panzoom("pan", 0, -30, { relative: true });
-    } else if (key==38) { //Up key
+    } else if (key==38 && book.type=="images") { //Up key
       $('#panzoom').panzoom("pan", 0, 30, { relative: true });
-    } else if (key==37) { //Left key
+    } else if (key==37 && book.type=="images") { //Left key
       $('#panzoom').panzoom("pan", 30, 0, { relative: true });
-    } else if (key==39) { //Right key
+    } else if (key==39 && book.type=="images") { //Right key
       $('#panzoom').panzoom("pan", -30, 0, { relative: true });
     }
   })
